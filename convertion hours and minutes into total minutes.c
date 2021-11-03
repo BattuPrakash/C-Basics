05d94b0da2dad6151794da2b5b1fbdd34f655812
BattuPrakash <92287891+BattuPrakash@users.noreply.github.com>
2021-11-03 10:21:32 +0530

    Add files via upload

---
+++ convertion hours and minutes into total minutes.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;	
+a=9;
+b=10;
+c=a*60+b;
+
+printf("%d hours and %d minutes is equal to %d minutes",a,b,c);
+}
+
+
+
+
+
+
+
+
+
+
---
+++ covertion of minutes into hours and minutes.c
@@ -0,0 +1,16 @@
+//c program that convertes minutes into hours and minutes
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+ 
+ 
+a= 550;
+b= a/60;
+c= a%60;
+
+
+printf(" %d minutes is equal  %d hour and is %d minutes",a,b,c);
+}
+
+
