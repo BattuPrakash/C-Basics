23c740a02082b876b5c7ea54b52953baacf2ca86
BattuPrakash <92287891+BattuPrakash@users.noreply.github.com>
2021-12-06 10:46:19 +0530

    Add files via upload

---
+++ coditional statements(if,else).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter a number check whether it is even or odd",n);
+    scanf("%d",&n);
+    if(n%2==0)
+   {
+   	printf("%d is even",n);
+   	
+   }
+   else
+   {
+   	
+   	printf("%d is odd",n);
+   }
+
+}
---
+++ conditional statements(if,else)1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter a number to check whether it is positive or negative",n);
+	scanf("%d",&n);
+	if (n>0)
+	{
+		printf("%d is positive",n);
+		
+	}
+	else
+	{
+		printf("%d is negative",n);
+	}
+}
